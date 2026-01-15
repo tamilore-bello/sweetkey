149466ed3dc87a8d5d43ac5e9a01b560aeeefed5
miloticaspx <Hazeltail13@gmail.com>
2026-01-15 15:07:36 -0600

    code cleaning

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,47 @@
-package org.example;
 
 import java.sql.*;
 import java.util.*;
+import org.example.*;
 
+void main() {
+    UserDAO ud = new UserDAO();
+    ud.rundb();
+    ud.fetchAllUsers();
+}
 
-public class Main {
-    public void main() {
-        // init scanner object
-        Scanner scanner = new Scanner(System.in);
-
-        // instance variables
-        String username;
-        String password;
-
-        System.out.println("ENTER USERNAME");
-        username = scanner.nextLine();
-        System.out.println("ENTER PASSWORD ");
-        password = scanner.nextLine();
-
-        User user = new User(username, password);
-        System.out.println(user.getUsername() + "\n" + user.getDate_joined());
-        Commission commission = new Commission("sdad", "smoothsailor23", "bust", "n/a", true);
-
-        UserDAO ud = new UserDAO();
-        ud.rundb();
-        ud.addUser(user);
-        ud.fetchAllUsers();
-
-    }
-
+public void addCommission() {
+    User user = new User("jess", "jessisawesome");
+    Scanner scanner = new Scanner(System.in);
+    String username;
+    String password;
+    Commission commission = new Commission(
+            user.getUuid(),
+            "smoothsailor23",
+            "bust",
+            "n/a",
+            true);
+    UserDAO ud = new UserDAO();
 
 }
+
+public void addUser() {
+    Scanner scanner = new Scanner(System.in);
+    String username;
+    String password;
+
+    System.out.println("ENTER USERNAME");
+    username = scanner.nextLine();
+    System.out.println("ENTER PASSWORD ");
+    password = scanner.nextLine();
+
+    User user = new User(username, password);
+
+    UserDAO ud = new UserDAO();
+    ud.addUser(user);
+}
+
+
+
 
 
 
