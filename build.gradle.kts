f6ad0bfc82e8be044f4387022f916dd75d5ec463
miloticaspx <Hazeltail13@gmail.com>
2026-01-19 22:53:05 -0600

    created a swing page for the log in page (no functionality yet.)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -8,6 +9,7 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,121 @@
 
-import java.sql.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
-import java.util.Date;
-
 import org.example.*;
+
+import javax.swing.*;
 
 void main() {
     // get the database up and running ! we create a UserDAO object, through which we can access various
     // database methods. The first thing we do is run the database.
     DevUtils du = new DevUtils();
     UserDAO ud = new UserDAO();
-     ud.rundb();
+    ud.rundb();
 
      // for prototyping and development purposes, we use the internal methods to print all users and all
      // commissions
-     printAllUsers();
-     printAllComms();
+     //printAllUsers();
+     //printAllComms();
     // addUser();
-     welcomeUser();
+     //welcomeUser();
+
+    swingUI();
+}
 
 
+public void swingUI() {
+    Font defaultFont = new Font("SF Pro Rounded", Font.PLAIN, 16);
+    UIManager.put("Label.font", defaultFont);
+    JFrame jframe = new JFrame(" ♡ ✿ sweetkey ✿ ♡");
+
+    // overarching content pane in the jFrame
+    JPanel panel = new JPanel();
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    panel.setBorder(BorderFactory.createEmptyBorder(60, 60, 60, 60));
+    jframe.setContentPane(panel);
+
+
+    // below is components inside the frame !
+
+    // panel for welcome header + log in , everything except
+    // header and footer basically
+    JPanel group = new JPanel();
+    group.setLayout(new BoxLayout(group, BoxLayout.Y_AXIS));
+    group.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    // header
+    JPanel header_row = new JPanel();
+    header_row.setLayout(new BoxLayout(header_row, BoxLayout.X_AXIS));
+    JLabel header_text = new JLabel("Welcome!");
+    header_text.setFont(new Font("SF Pro Display", Font.BOLD, 40));
+    header_row.add(header_text);
+
+    // username row
+    JPanel username_row = new JPanel();
+    username_row.setLayout(new BoxLayout(username_row, BoxLayout.X_AXIS));
+
+    username_row.setAlignmentX(Component.CENTER_ALIGNMENT);
+    JLabel username_text = new JLabel("Username: ");
+    JTextField username_field = new JTextField(15);
+    username_field.setMaximumSize(new Dimension(username_field.getPreferredSize().width, username_field.getPreferredSize().height));
+    username_row.add(username_text);
+    username_row.add(username_field);
+
+    // password row
+    JPanel password_row = new JPanel();
+    password_row.setLayout(new BoxLayout(password_row, BoxLayout.X_AXIS));
+    password_row.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    JLabel password_text = new JLabel("Password: ");
+    JPasswordField password_field = new JPasswordField(15);
+    password_field.setMaximumSize(new Dimension(password_field.getPreferredSize().width, password_field.getPreferredSize().height));
+    password_row.add(password_text);
+    password_row.add(password_field);
+
+    // Button row
+    JPanel buttonRow = new JPanel();
+    buttonRow.setLayout(new BoxLayout(buttonRow, BoxLayout.X_AXIS));
+    buttonRow.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    JButton button = new JButton("Log In");
+    button.setFont(new Font("SF Pro Rounded", Font.PLAIN, 14));
+    Dimension buttonSize = new Dimension(280, button.getPreferredSize().height);
+    button.setMinimumSize(buttonSize);
+    button.setPreferredSize(buttonSize);
+    button.setMaximumSize(buttonSize);
+    buttonRow.add(Box.createHorizontalGlue());
+    buttonRow.add(button);
+    buttonRow.add(Box.createHorizontalGlue());
+
+    // on click action!
+    button.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("click registered! "+Date.from(Instant.now()));
+        }
+    });
+    // adding everything to the center container
+    group.add(header_row);
+    group.add(Box.createRigidArea(new Dimension(0, 20))); // 20px vertical space
+    group.add(Box.createRigidArea(new Dimension(0, 15))); // space between header and username
+    group.add(username_row);
+    group.add(Box.createRigidArea(new Dimension(0, 15))); // space between header and username
+    group.add(password_row);
+    group.add(Box.createRigidArea(new Dimension(0, 15))); // space between header and username
+    group.add(buttonRow);
+
+    // addint the center container to the overarching panel
+    panel.add(Box.createVerticalGlue());
+    panel.add(group);
+    panel.add(Box.createVerticalGlue());
+    panel.add(Box.createVerticalGlue());
+
+    // fixes to the frame itself
+    jframe.setBounds(200, 100, 100, 100);
+    jframe.setSize(600,800);
+    jframe.setVisible(true);
 }
 
 // all the current user to log in
@@ -52,6 +148,7 @@ public void printAllUsers() {
         System.out.println(i);
     }
 }
+
 
 @InternalMethod
 @Deprecated
